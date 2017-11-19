03e7fbf966bb748d5ba6b240249f4371fe4955f1
DefNeoRoman <d89056411060@yandex.ru>
2017-11-19 07:51:06 +0300

    remove old files

--- src/main/java/com/example/LevelCache/SimpleDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LevelCache;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimpleDataRepository extends JpaRepository<SimpleData, Long> {
-
-}
